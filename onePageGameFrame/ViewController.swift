31fbabad117c36262a083db820105917929d69c5
李炜 <winxos@icloud.com>
2016-09-21 00:25:39 +0800

    Initial Commit

---
+++ onePageGameFrame/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  onePageGameFrame
+//
+//  Created by www on 16/9/21.
+//  Copyright © 2016年 www. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
